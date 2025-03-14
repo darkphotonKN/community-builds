2fabcea3db489b9ffff1d02685473ef1c0972d1a
Kranti <darkphoton20@gmail.com>
2025-03-14 15:55:17 +0800

    feat: Publish builds further refactor.

--- config/routes.go
+++ config/routes.go
@@ -133,6 +133,7 @@ func SetupRouter() *gin.Engine {
 	articleRepo := article.NewArticleRepository(DB)
 	articleService := article.NewArticleService(articleRepo)
 	articleHandler := article.NewArticleHandler(articleService)
+
 	// -- Article Routes --
 	articleRoutes := api.Group("/article")
 
